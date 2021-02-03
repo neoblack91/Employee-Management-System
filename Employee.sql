bbd87d2b93764cc7783da2de9bdb60a05cc596a9
Rajahni fields <rajahnifields@gmail.com>
2021-02-03 00:29:35 -0500

    added connection

---
+++ Employee.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+	id INT ,
+    name VARCHAR(30) NOT NULL ,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE  role(
+id INT NOT NULL,
+title VARCHAR (30) NOT NULL,
+salary DECIMAL (10,2) NOT NULL,
+department_id INT NOT NULL, 
+PRIMARY KEY (id)
+) ;
+
+CREATE TABLE employee(
+	id INT NOT NULL ,
+  first_name  VARCHAR(30) NOT NULL , 
+  last_name  VARCHAR(30)NOT NULL , 
+  role_id  INT NOT NULL , 
+  manager_id  INT NOT NULL ,
+  PRIMARY KEY (id)
+)
---
+++ employee.js
@@ -0,0 +1,22 @@
+
+const mysql = require('mysql');
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  port: 3306,
+
+  
+  user: 'root',
+
+    password: '',
+
+  
+  database: 'employee_db',
+})
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  connection.end();
+});

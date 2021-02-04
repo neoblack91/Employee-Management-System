1a624a46e86df73a76632b2683ea144e7c9b6918
Rajahni fields <rajahnifields@gmail.com>
2021-02-03 20:55:20 -0500

    working on something

--- employee.js
+++ employee.js
@@ -1,22 +1,16 @@
-
 const mysql = require('mysql');
 
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-
-  port: 3306,
-
-  
-  user: 'root',
-
-    password: '',
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//     password: '',
 
   
-  database: 'employee_db',
-})
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
-  connection.end();
-});
+//   database: 'employee_db',
+// })
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('connected as id ' + connection.threadId);
+//   connection.end();
+// });

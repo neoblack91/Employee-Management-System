2b468fa673fe68b395118c5507920ca05cc3f4b0
Rajahni fields <rajahnifields@gmail.com>
2021-02-09 01:54:06 -0500

    yay I can add a role

--- employee.js
+++ employee.js
@@ -138,79 +138,74 @@ const editDepartment = () => {
     connect.query("SELECT * FROM department", function (err, res) {
       if (err) throw err;
       var departResult = res;
-  
+
       departments = departResult.map((seeDEP) => {
         return seeDEP.title;
       });
     });
-    inquirer.prompt([
-      {
-        type: "list",
-        name: "employeethings",
-        message: "Who would you like to edit?",
-        choices: employChoice,
-      },
-    ])
-    
-    .then((response) => { 
-     
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employeethings",
+          message: "Who would you like to edit?",
+          choices: employChoice,
+        },
+      ])
 
+      .then((response) => {
         switch (response.employeethings) {
           case seeDEP.title:
             inquirer.prompt([
               {
                 type: "list",
                 name: "employeethings",
-                message: "Which department you want to assign to this employee?",
+                message:
+                  "Which department you want to assign to this employee?",
                 choices: departments,
               },
             ]);
-  
+
           default:
             break;
         }
       });
-
   });
-   
 };
 
 const editRole = () => {
-  
   connect.query("SELECT *FROM employee", function (err, res) {
     if (err) throw err;
     var employroResult = res;
     var employChoice = employroResult.map((seeEmploy) => {
       return seeEmploy.first_name + " " + seeEmploy.last_name;
     });
-    
+
     connect.query("SELECT * FROM role", function (err, res) {
       if (err) throw err;
       var roleResult = res;
-  
+
       var roleChoice = roleResult.map((seeDEP) => {
         return seeDEP.title;
       });
-    
-    inquirer.prompt([
-      {
-        type: "list",
-        name: "employeethings",
-        message: "Who would you like to edit?",
-        choices: employChoice,
-      },
-      {
-        type: "list",
-        name: "employeethings",
-        message: "What role you want to change?",
-        choices: roleChoice,
-      },
-    ]).then((response)=>{
 
-
-
-    })
-  });
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "employeethings",
+            message: "Who would you like to edit?",
+            choices: employChoice,
+          },
+          {
+            type: "list",
+            name: "employeethings",
+            message: "What role you want to change?",
+            choices: roleChoice,
+          },
+        ])
+        .then((response) => {});
+    });
   });
 };
 
@@ -234,21 +229,42 @@ const departmentStuff = () => {
 };
 
 const roleStuff = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "roleName",
-        message: "Enter new role:",
-        validate: confirmString,
-      },
-    ])
-    .then((response) => {
-      connect.query(`INSERT role.id, title, salary, department.name AS department
-      FROM role INNER JOIN department ON role.department_id = department.id` [response.roleName]);
-      console.log(`${response.roleName} was added to roles.`);
-      MainMenu();
+  connect.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.log(" ");
+    console.table("List of departments", res);
+    const departmentResults = res;
+    const departmentChoices = departmentResults.map((departmentResult) => {
+      return departmentResult.name;
     });
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "roleName",
+          message: "Enter new role:",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "Enter the salary:",
+        },
+        {
+          type: "list",
+          name: "departName",
+          message: "What department is this role in?",
+          choices: departmentChoices,
+        },
+      ])
+      .then((response) => {
+        connect.query(
+          `INSERT INTO role(title, salary, department_id) VALUES ("${response.roleName}", "${response.salary}", (SELECT id FROM department WHERE name = "${response.departName}"))`
+        );
+        console.log(`${response.roleName} was added to roles.`);
+        MainMenu();
+      });
+  });
 };
 
 const Deletestuff = () => {
@@ -297,12 +313,12 @@ function getAllEmployee() {
   );
 }
 
-function confirmString(input) {
-  if (input.trim() != "" && input.trim().length <= 30) {
-    return true;
-  }
-  return "Invalid input. Please limit your input to 30 characters or less.";
-}
+// function confirmString(input) {
+//   if (input.trim() != "" && input.trim().length <= 30) {
+//     return true;
+//   }
+//   return "Invalid input. Please limit your input to 30 characters or less.";
+// }
 
 MainMenu = () => {
   return questions();
@@ -344,35 +360,39 @@ const addEmployee = () => {
         if (err) throw err;
         var roleResult = res;
         roleChoice = roleResult.map((seerol) => {
-          return seerol.name;
+          return seerol.title;
         });
+
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "addfirst",
+              message: "What is their first name?",
+            },
+            {
+              type: "input",
+              name: "addlast",
+              message: "What is their last name?",
+            },
+            {
+              type: "list",
+              name: "addepartment",
+              message: "What department are they in?",
+              choices: departChoice,
+            },
+            {
+              type: "list",
+              name: "addrole",
+              message: "What is their Role?",
+              choices: roleChoice,
+            },
+          ])
+          .then((response)=>{
+
+            
+          }
       }
     );
-    inquirer.prompt([
-      {
-        type: "input",
-        name: "addfirst",
-        message: "What is their first name?",
-      },
-      {
-        type: "input",
-        name: "addlast",
-        message: "What is their last name?",
-      },
-      {
-        type: "list",
-        name: "addepartment",
-        message: "What department are they in?",
-        choices: departChoice,
-      },
-      {
-        type: "list",
-        name: "addrole",
-        message: "What is their Role?",
-        choices: roleChoice,
-      },
-    ]);
   });
 };
-
-
